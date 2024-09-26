5f626afdac2368f38b6133edcd4e5958719fb43b
haiq0001 <haiq0001@stud.kea.dk>
2024-09-26 13:27:55 +0200

    Adventure Game

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+        public static void main(String[] args) {
+            Adventure adventure = new Adventure();
+            adventure.play();
+        }
+    }
+
---
+++ src/Room.java
@@ -0,0 +1,50 @@
+public class Room {
+    private String name;
+    private String description;
+    private Room north, south, east, west;
+
+    public Room(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Room getNorth() {
+        return north;
+    }
+
+    public void setNorth(Room north) {
+        this.north = north;
+    }
+
+    public Room getSouth() {
+        return south;
+    }
+
+    public void setSouth(Room south) {
+        this.south = south;
+    }
+
+    public Room getEast() {
+        return east;
+    }
+
+    public void setEast(Room east) {
+        this.east = east;
+    }
+
+    public Room getWest() {
+        return west;
+    }
+
+    public void setWest(Room west) {
+        this.west = west;
+    }
+}
