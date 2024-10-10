8f8023fee4cb43cc54e32cd1c5fc25fc5174fc7f
haiq0001 <haiq0001@stud.kea.dk>
2024-10-10 17:03:32 +0200

    Delete src directory

--- src/Weapon.java
+++
@@ -1,8 +0,0 @@
-public abstract class Weapon extends Item {
-    public Weapon(String name) {
-        super(name);
-    }
-
-    public abstract boolean canUse();
-    public abstract void use();
-}
